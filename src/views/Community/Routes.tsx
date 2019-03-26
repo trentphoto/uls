274fc1208cbe05d1079a5aaadc42da9430ce3ed5
James Trent <trentphoto@gmail.com>
2019-03-26 17:42:17 -0400

    add store page

--- src/views/Community/Routes.tsx
+++ src/views/Community/Routes.tsx
@@ -3,6 +3,7 @@ import { RouteComponentProps, Switch, Route } from 'react-router'
 import Community from './Community'
 import Calendar from './Calendar'
 import Updates from './Updates'
+import Store from './Store'
 
 interface Props extends RouteComponentProps {}
 
@@ -14,6 +15,7 @@ const CommunityRoutes = ({ match, location }: Props) => (
       path={`${match.path}/calendar-of-events`}
       component={Calendar}
     />
+    <Route exact path={`${match.path}/store`} component={Store} />
     <Route exact path={`${match.path}/community-updates`} component={Updates} />
   </Switch>
 )
